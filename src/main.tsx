0dda32270395ab6c460560c857aa3ae923a10097
Julia Murashko <murashkoyuliua@gmail.com>
2026-06-10 14:39:43 +0300

    Add Mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 
-createRoot(document.getElementById('root')!).render(
+import "@mantine/core/styles.css";
+
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
