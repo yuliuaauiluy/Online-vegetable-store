261dd24689b0cddd45abde34080f52c20db1c69b
Julia Murashko <murashkoyuliua@gmail.com>
2026-06-24 22:39:13 +0300

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Online-vegetable-store/",
   plugins: [react()],
   test: {
     environment: "jsdom",
